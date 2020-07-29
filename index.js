46aa663d98c26d304b75764afd8c11c5cbb641a9
Nir Leibovitch <nir@giffgaff.co.uk>
2020-07-29 20:32:46 +0100

    re factor index

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const Main = require('./src/main');
 const Logger = require('./src/logger');
 const Utils = require('./src/utils');
-const Files = require('./src/files');
 const path = require('path');
 
 process.on('unhandledRejection', (reason, promise) => {
     Logger.error(`Unhandled Rejection\n${reason.stack || reason}`)
 })
 
-async function main() {
+function fetch_args() {
     let args = Utils.cmd_string_to_obj_args();
-    // let defaults = {
-    //     project_path: "src/project",
-    //     changed_file_path: "src/mocks/CHANGED.txt"
-    // };
     let defaults = {
         project_path: "sample-project",
         changed_file_path: "CHANGED.txt"
     };
-    args = Object.assign(defaults, args);
+    return Object.assign(defaults, args);
+}
+
+async function main() {
+    const args = fetch_args();
 
     const project_path = path.join(__dirname, args.project_path);
     const changed_file_path = path.join(__dirname, args.changed_file_path);
